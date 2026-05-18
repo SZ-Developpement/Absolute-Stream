57ce9ee7b2db2893ae7185cb954b76b5bc7cceca
thomas-montout <montoutthomas@gmail.com>
2026-05-18 16:09:03 +0200

    filter: add of filter logic

--- src/components/library/DiscoverMovies.tsx
+++ src/components/library/DiscoverMovies.tsx
@@ -16,9 +16,11 @@ export function DiscoverMovies({
 }) {
   const [movies, setMovies] = useState<Media[]>(initialMovies); // On initialise l'état des films avec les films passés en props pour éviter un écran vide au chargement initial.
   const [selectedGenre, setSelectedGenre] = useState<string>(""); // État pour stocker le genre sélectionné
-  const [sortBy, setSortBy] = useState<string>("popularity.desc");
+  const [sortBy, setSortBy] = useState<string>(""); // État pour stocker le critère de tri sélectionné
   const [isPending, startTransition] = useTransition(); // Hook pour gérer les transitions d'état sans bloquer l'interface utilisateur
   const isInitialRender = useRef(true); // Ref pour suivre si c'est le premier rendu du composant, ref = une valeur mutable qui persiste entre les rendus sans provoquer de re-render lorsqu'elle change
+  const [isGenreOpen, setIsGenreOpen] = useState(false); // État pour gérer l'ouverture du menu de sélection de genre
+  const [isSortOpen, setIsSortOpen] = useState(false); // État pour gérer l'ouverture du menu de sélection de tri
 
   // useEffect pour déclencher une nouvelle requête à l'API TMDB chaque fois que le genre sélectionné ou le critère de tri change, mais pas au premier rendu grâce à isInitialRender
   useEffect(() => {
@@ -92,7 +94,7 @@ export function DiscoverMovies({
           <select
             id="sort-select"
             value={sortBy}
-            onChange={(e) => setSortBy(e.target.value)}
+            onChange={
             className="p-2 text-sm rounded-md bg-background/50 border border-foreground/20 text-foreground focus:ring-2 focus:ring-primary focus:border-transparent w-58"
           >
             <option value="popularity.desc">Popularité Décroissante</option>
@@ -107,28 +109,133 @@ export function DiscoverMovies({
         <div className="flex flex-row gap-2 items-center ">
           <button
             onClick={() => {
-              setSelectedGenre("");
-              setSortBy("popularity.desc");
+              setIsGenreOpen((open) => !open);
             }}
-            className="w-full whitespace-nowrap py-2 px-4 flex flex-row justify-center items-center gap-2 text-sm text-center rounded-lg text-white hover:text-gray-300 bg-background/70 hover:bg-background/60 cursor-pointer"
+            className="w-full relative whitespace-nowrap py-2 px-4 flex flex-row justify-center items-center gap-2 text-sm text-center rounded-lg text-white hover:text-gray-300 bg-background/70 hover:bg-background/60 cursor-pointer"
           >
             <ListFilter size={14} />
             Filtrez par Genre
           </button>
 
-          <button
-            onClick={() => {
-              setSelectedGenre("");
-              setSortBy("popularity.desc");
-            }}
-            className="w-full whitespace-nowrap py-2 px-4 flex flex-row justify-center items-center gap-2 text-sm text-center rounded-lg text-white hover:text-gray-300 bg-background/70 hover:bg-background/60 cursor-pointer"
-          >
-            <ArrowDownUp size={14} />
-            Triez
-          </button>
+          {isGenreOpen && (
+            <ul
+              className="absolute mb-75 right-45 w-48 max-h-64 overflow-y-auto rounded-lg bg-background border border-foreground/20
+  z-10"
+            >
+              <li>
+                <button
+                  onClick={() => {
+                    setSelectedGenre("");
+                    setIsGenreOpen(false);
+                  }}
+                  className="w-full px-3 py-2 hover:bg-foreground/10"
+                >
+                  Tous
+                </button>
+              </li>
+              {genres.map((genre) => (
+                <li key={genre.id}>
+                  <button
+                    onClick={() => {
+                      setSelectedGenre(genre.id.toString());
+                      setIsGenreOpen(false);
+                    }}
+                    className="w-full text-left px-3 py-2 hover:bg-foreground/10"
+                  >
+                    {genre.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+
+          <div className="flex flex-row gap-2 items-center ">
+            <button
+              onClick={() => {
+                setIsSortOpen((open) => !open);
+              }}
+              className="w-full whitespace-nowrap py-2 px-4 flex flex-row justify-center items-center gap-2 text-sm text-center rounded-lg text-white hover:text-gray-300 bg-background/70 hover:bg-background/60 cursor-pointer"
+            >
+              <ArrowDownUp size={14} />
+              Triez
+            </button>
+
+            {isSortOpen && (
+              <ul
+                className="absolute mb-75 right-45 w-48 max-h-64 overflow-y-auto rounded-lg bg-background border border-foreground/20
+  z-10"
+              >
+                <li>
+                  <button
+                    onClick={() => {
+                      setSortBy("popularity.desc");
+                      setIsSortOpen(false);
+                    }}
+                    className="w-full px-3 py-2 hover:bg-foreground/10"
+                  >
+                    Popularité Décroissante
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      setSortBy("popularity.asc");
+                      setIsSortOpen(false);
+                    }}
+                    className="w-full px-3 py-2 hover:bg-foreground/10"
+                  >
+                    Popularité Croissante
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      setSortBy("vote_average.desc");
+                      setIsSortOpen(false);
+                    }}
+                    className="w-full px-3 py-2 hover:bg-foreground/10"
+                  >
+                    Note Décroissante
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      setSortBy("vote_average.asc");
+                      setIsSortOpen(false);
+                    }}
+                    className="w-full px-3 py-2 hover:bg-foreground/10"
+                  >
+                    Note Croissante
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      setSortBy("primary_release_date.desc");
+                      setIsSortOpen(false);
+                    }}
+                    className="w-full px-3 py-2 hover:bg-foreground/10"
+                  >
+                    Plus Récents
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      setSortBy("primary_release_date.asc");
+                      setIsSortOpen(false);
+                    }}
+                    className="w-full px-3 py-2 hover:bg-foreground/10"
+                  >
+                    Plus Anciens
+                  </button>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
-
       <div
         className={`justify-items-center grid grid-cols-2 xl:grid-cols-6 2xl:grid-cols-8 gap-4 transition-opacity ${
           isPending ? "opacity-50" : "opacity-100"
